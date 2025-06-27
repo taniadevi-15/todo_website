8ca7c7b04c60a4f0a36db2b6224f95b751a6d01e
tania <taniarajput303@gmail.com>
2025-06-27 22:05:37 +0530

    first commit

---
+++ backend/routes/todo.route.js
@@ -0,0 +1,18 @@
+
+import express from "express";
+import {
+  createTodo,
+  deleteTodo,
+  getTodos,
+  updateTodo,
+} from "../controller/todo.controller.js";
+import { authenticate } from "../middleware/authorise.js";
+
+const router = express.Router();
+
+router.post("/create", authenticate, createTodo);
+router.get("/fetch", authenticate, getTodos);
+router.put("/update/:id", authenticate, updateTodo);
+router.delete("/delete/:id", authenticate, deleteTodo);
+
+export default router;
---
+++ frontend/src/components/PageNotFound.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const PageNotFound = () => {
+  return (
+   <>
+   <div className='  flex flex-col items-center justify-center h-screen'>
+    <div className='text-2xl'>404</div>
+    <div className='text-xl font-semibold text-emerald-950'>Page Not Found</div>
+   </div>
+   </>
+  )
+}
+
+export default PageNotFound

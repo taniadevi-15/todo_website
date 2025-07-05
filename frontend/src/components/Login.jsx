6b53aa0c6ef57b08cb136b4f95e3a7f2eafb6ab1
Tania Devi <taniarajput303@gmail.com>
2025-07-05 22:05:01 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -12,7 +12,7 @@ function Login() {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:4001/user/login",
+        "https://todo-websitee.onrender.com/user/login",
         { email, password },
         {
           withCredentials: true,
